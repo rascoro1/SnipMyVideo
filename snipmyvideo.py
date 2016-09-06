3b38da2139b7eeb279fd02a8d61edbb60d979f81
Rascoro1 <copelanda1@wit.edu>
2016-09-05 23:09:29 -0400

    Spelling Errors, Moved Argument Checking Condition
    Fixed spelling errors and added verbose notes.
    Moved the argument checking conditional statement into the main function,

--- snipmyvideo.py
+++ snipmyvideo.py
@@ -6,30 +6,27 @@ import os
 """
 
 This script requires moviepy.py
-    Is can be obtained through pip:
+    It can be obtained through pip:
         pip install moviepy
 
-Output file can be '.mp4', 'ogv', 'webm'
+Output file can be 'mp4', 'ogv', 'webm'
     You can modify script to output other files based off the codec.
         Further reasearch into moviepy.VideoFileClip.write_videofile
 
 Usage: SnipMyVideo.py video.mp4 output.mp4 30-60 90-120
     E.g: This would create two snipets (unlimited snipets can be created),
-        the first snipet resembles the time 30seconds to 60 seconds from video.m94
+        the first snipet resembles the time 30seconds to 60 seconds from video.mp4
+        the second snipet is the video for the time 90- 120 in the video.mp4
         These snipets are then concatenated and written to output.mp4
 
 
 
 Only a couple lines actually using moviepy.
-This is a simple script that can only snip your code and return it back in one video
+This is a simple script that can only snip your video and return it back in one video
 
 """
 
-if len(sys.argv) < 4:
-    print "You have not given enough Arguments"
-    print "usage: " + sys.argv[0] + " inputfile.mp4 outputfile.mp4 20-30"
-    print "Time is in seconds"
-    sys.exit(6)
+
 
 SCRIPT_NAME = sys.argv[0]
 args = sys.argv[1:]  # Trimming of SCRIPT_NAME
@@ -86,6 +83,12 @@ def create_viedo(snipets):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print "You have not given enough Arguments"
+        print "usage: " + sys.argv[0] + " inputfile.mp4 outputfile.mp4 20-30"
+        print "Time is in seconds"
+    
+    sys.exit(6)
     check_args()
     snipets = get_snipets()
     create_viedo(snipets)
