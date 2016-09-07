dd475d8c8057733533a75e2f5ee2a432f313bb1b
Rascoro1 <copelanda1@wit.edu>
2016-09-06 22:50:34 -0400

    Verbosity, hour:min:sec
    Added function for verbose logging called verbose(). hour:min:sec and min:sec format are available and will be converted to seconds. Added a lot of error checking with time to only allow appropriate time.

--- snipmyvideo.py
+++ snipmyvideo.py
@@ -4,37 +4,64 @@ import sys
 import os
 
 """
+This is a simple script that can only snip your code and return it back in one video
 
 This script requires moviepy.py
-    It can be obtained through pip:
+    Is can be obtained through pip:
         pip install moviepy
 
-Output file can be 'mp4', 'ogv', 'webm'
+Output file can be '.mp4', 'ogv', 'webm'
     You can modify script to output other files based off the codec.
         Further reasearch into moviepy.VideoFileClip.write_videofile
 
-Usage: SnipMyVideo.py video.mp4 output.mp4 30-60 90-120
+Usage: SnipMyVideo.py video.mp4 output.mp4 30-60 90-120 20:20-20:40 1:20:15-1:30:15
     E.g: This would create two snipets (unlimited snipets can be created),
-        the first snipet resembles the time 30seconds to 60 seconds from video.mp4
-        the second snipet is the video for the time 90- 120 in the video.mp4
+        the first snipet resembles the time 30seconds to 60 seconds from video.m94
         These snipets are then concatenated and written to output.mp4
 
 
 
 Only a couple lines actually using moviepy.
-This is a simple script that can only snip your video and return it back in one video
+
 
 """
 
+###################### Declaring Globals ##################################
+SCRIPT_NAME = "" # Name of the script
+FNAME = "" # Input filename
+OUT_FNAME = "" # OUtput filename
+SNIPPETS = [] # Raw Snippet times given as arguments by the user
+SNIPPET_TIMES = [] # Snippet times in seconds
+VERBOSE = True # Turn to false if you would not like verbose information
+
+def check_num_of_arguments():
+    """
+    Quick check to make sure the correct number of argumets are portrayed.
+    First function that is called
+    :return:
+    """
+    if len(sys.argv) < 4:
+        print "You have not given enough Arguments"
+        print "usage: " + sys.argv[0] + " inputfile.mp4 outputfile.mp4 20-30"
+        print "Time is in seconds"
+        sys.exit(6)
 
 
-SCRIPT_NAME = sys.argv[0]
-args = sys.argv[1:]  # Trimming of SCRIPT_NAME
-FNAME = args[0]
-args = args[1:]  # Timming off fname
-OUT_FNAME = args[0]
-ARGS = args[1:] # Trimming off the output filename
-del args # Delete old args (REPLACED BY GLOBAL)
+def trim_arguments():
+    """
+    This trims all of the input arguments from the user
+    and assagins information to the according global variable
+    :return:
+    """
+    global SCRIPT_NAME, FNAME, OUT_FNAME, SNIPPETS
+    SCRIPT_NAME = sys.argv[0]
+    args = sys.argv[1:]  # Trimming of SCRIPT_NAME
+    FNAME = args[0]
+    args = args[1:]  # Timming off fname
+    OUT_FNAME = args[0]
+    SNIPPETS = args[1:]  # Trimming off the output filename
+    del args  # Delete old args (REPLACED BY GLOBAL)
+
 
 def check_args():
     """
@@ -51,82 +78,145 @@ def check_args():
 
     if not os.path.isdir(os.path.abspath(OUT_FNAME).rstrip(os.path.basename(OUT_FNAME))):
         print "Output file directory does not exists"
+
+
         sys.exit(3)
 
-
-def convert_to_sec(inp_list):
-    """ expects format as [hour, min, sec] or [min, sec] or [sec], returns seconds as int """
-    if len(inp_list) == 1:
-        return int(inp_list[0])
-    elif len(inp_list) == 2:
-        return int(inp_list[0]) * 60 + int(inp_list[1])
-    elif len(inp_list) == 3:
-        return int(inp_list[0]) * 3600 + int(inp_list[1]) * 60 + int(inp_list[2])
-
-
-def get_snipets():
+def check_time(time, min, max):
     """
-    Input: list of str of time range in format of sec, min:sec, or hour:min:sec
-    Process: each value from input list becomes start and stop time in seconds as int
-    Output: list of objects of VideoFileClip.subclip(start, stop)
+    Check time submitted by user to make sure time is correct and follows a correct format
+    :param time: time requested by user
+    :param min: min possible value
+    :param max: max possible value
+    :return:
     """
-    snipets = []
-    # e.g. input: ['6-12:06', '12:10-12:30', '0:50:22-1:10:0']
-    # iterate through each input time range / snipet
-    for arg in ARGS:
-        if '-' in arg:
-            time = [[x] for x in arg.split('-', 1)] # '6-12:06' => [['6'], ['12:06']]
-            if len(time) != 2:
-                print('Input needs to contain start & stop times with \'-\' between to indicate time range for input video, see README.md')
-                sys.exit(7)
+    if int(time) >= min and int(time) <= max:
+        return True
+    else:
+        return False
 
-            # iterate through start then stop times
-            for index in range(len(time)):
-                if ':' in time[index][0]:
-                    if time[index].count(':') > 2:
-                        print('There shouldn\'t be more than 2 \':\' per start or stop time entered.')
-                        sys.exit(8)
-                    time[index] = time[index][0].split(':') # [['6'], ['12:06']] => [['6'], ['12', '06']]
-                    # print('time[%s] after ":" split: %s' % (index, time[index])) # debug
-                # check to see if snippets are valid
-                for value in time[index]:
-                    if not value.isdigit():
-                        print "Input must be a number"
-                        sys.exit(4)
-                    if int(value) < 0:
-                        print "Input must be a positive number"
-                        sys.exit(5)
-                # convert to seconds
-                time[index] = convert_to_sec(time[index]) # [['6'], ['12', '06']] => [6, 726]
-                # print('time[%s] after sec conversion: %s' % (index, time[index])) # debug
+def convert_human_readable_time(time):
+    """
+    Convert human readable format of time into seconds
 
-            start, stop = time[0], time[1]
-            if start > stop:
-                print('Start needs to be smaller than stop for snipet #%s, exiting.' % (index))
-                sys.exit(6)
-            snipets.append(VideoFileClip(FNAME).subclip(int(start), int(stop)))
-            # print "Created Snippet:\n\tStarting: %s STOPPING: %s " % (start, stop) # debug
+    :param time: human readable string format of 1:20:30 or 20:38 or 38
+    :return: int time in seconds
+    """
+    if ':' in time:  # The time follows the min:sec format and must be converted
+        time = time.split(':')
+        if len(time) == 2:  # Min:Sec format
+            min, sec = time
+            verbose("convert_human_readable_time("+str(time)+")",  "Min:Sec Format")
+            if check_time(min, 0, 59) and check_time(sec, 0, 59): # Making sure the times are between possible amounts
+                verbose("convert_human_readable_time(" + str(time) + ")", "time = " + str(min) + " * 60 + " + sec)
+                time = int(min) * 60 + int(sec)
+            else:
+                print("Incorrect Time has been submitted: " + str(time) + " min:sec 0:0-59:60")
+                sys.exit(10)
 
-        elif '-' not in arg:
-            print('Input needs to contain \'-\' to indicate time range for input video, see README.md')
-            sys.exit(7)
+        elif len(time) == 3:  # Hour:Min:Sec format
+            hour, min, sec = time
+            verbose("convert_human_readable_time("+str(time)+")",  "Hour:Min:Sec Format")
+            if check_time(hour, 0, 23) and check_time(min, 0, 59) and check_time(sec, 0, 59): # Making sure the times are between possible amounts
+                verbose("convert_human_readable_time(" + str(time) + ")", "time = " + str(hour) + " * 3600 + " + str(min) + " * 60 + " + sec)
+                time = int(hour) * 3600 + int(min) * 60 + int(sec)
+            else:
+                print("Incorrect Time has been submitted: " + str(time) + " hour:min:sec 0:0:0-23:59:59")
+                sys.exit(10)
 
-    return snipets
 
-def create_viedo(snipets):
-    video = concatenate(snipets)
+    try:
+        time = int(time)
+    except ValueError as e:
+        print "Value Error: Given time is not a digit" + e.message
+        sys.exit(8)
+
+    verbose("convert_human_readable_time(" + str(time) + ")", "Returned time is -> " + str(time))
+    return time # If the time  does not need to be converted (does not contain ':') it will still be appened
+
+def get_snippet_time(snippet):
+    """
+    This allows for easier use of snipping longer videos
+    conversion of
+        hour:min:sec & min:sec & sec e.g 1:20:15 & 20:48 & secs
+        to seconds
+
+    :param args: One snippet of time start and end time
+    :return: Dict of snippet e.g. {'start':20, 'stop': 40}
+    """
+    if "-" not in snippet: # Checking to see if snippet time was inputted correctly
+        print("The arguments for the snippet time is not in the correct format: " + snippet)
+        print("Correct usage is: 20-30 or 20:30-20:35 or 1:20:30-1:20:35 ")
+        sys.exit(7)
+
+    start, stop = snippet.split('-', 1)  # start and stop times of snippet
+    start = convert_human_readable_time(start)
+    stop = convert_human_readable_time(stop)
+    snippet = {'start': start, 'stop': stop}
+
+    return snippet
+
+def get_all_snippet_times():
+    """
+    Get all the snippet times in seconds
+    :return:
+    """
+    for snippet in SNIPPETS:
+        snippet = get_snippet_time(snippet)
+        for key in snippet:
+            if snippet[key] < 0:
+                print "Input must be a positive number"
+                sys.exit(5)
+
+        if snippet['start'] > snippet['stop']:
+            print('Start needs to be smaller than stop for snipet, exiting.')
+            verbose("check_args()", "start=" + str(snippet['start']) + ", stop="+str(snippet['stop']))
+            sys.exit(6)
+
+        SNIPPET_TIMES.append(snippet)
+
+def get_snippets():
+    """
+
+    :return: List of moviepy.subclip objects
+    """
+    snippets = []
+    for snippet in SNIPPET_TIMES:
+        snippets.append(VideoFileClip(FNAME).subclip(snippet['start'], snippet['stop']))
+        print "Created Snippet:\n\tStarting: " + str(snippet['start']) + " STOPPING: " + str(snippet['stop'])
+
+    return snippets
+
+def create_video(snippets):
+    """
+    Concatinate all the snippets together into one movie and write
+    :param snippets: THis is a list of moviepy.subclip
+    :return: write out one file
+    """
+    video = concatenate(snippets)
     print "Combined Snipets into one Video"
     print "Writing Video to " + OUT_FNAME
     video.write_videofile(OUT_FNAME)
 
 
+def verbose(title, info):
+    """
+    Loggin verbose notes when VERBOSE is True
+    Used for debuging and is very helpful
+    :param title:
+    :param info:
+    :return:
+    """
+    if VERBOSE:
+        print SCRIPT_NAME + " -> " + str(title) + ": " + info
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print "You have not given enough Arguments"
-        print "usage: " + sys.argv[0] + " inputfile.mp4 outputfile.mp4 20-30"
-        print "Time is in seconds"
-    
-    sys.exit(6)
-    check_args()
-    snipets = get_snipets()
-    create_viedo(snipets)
+    check_num_of_arguments() # Make sure we have the correct number of arguments
+    trim_arguments() # Trim the args to just have the snippets at the end
+    check_args() # check the args for correctness
+    get_all_snippet_times() # All snippet times in seconds and organized Also checked for correctness
+    snippets = get_snippets() # Get all the moviepy.subclip objects for each snippet
+    create_video(snippets) # Concatinate the Snippets together
+
+
